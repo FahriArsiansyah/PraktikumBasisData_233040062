57d89476027ec19ae314ef646a6ed059d0a14d1d
FahriArsiansyah <fahriarsiansyah@gmail.com>
2024-10-16 12:14:08 +0700

    LATIHAN2

---
+++ Pertemuan4/Latihan2/no.9.sql
@@ -0,0 +1,31 @@
+USE Latihan2
+
+select * from mahasiswa t
+WHERE t.tahun_masuk BETWEEN ('2019', '2020')
+
+SELECT * from mahasiswa t 
+WHERE t.jurusan 'teknik informatika' AND Jl.merdeka
+
+SELECT COUNT(*) jumlah_total_mahasiswa_informatika
+FROM Mahasiswa t
+WHERE 
+
+SELECT * FROM Mahasiswa 
+
+SELECT ORDER BY 
+
+SELECT * FROM mahasiswa 
+SELECT  tampilkan_nama_dosen
+
+
+SELECT * FROM mahasiswa
+
+
+SELECT kode_mata_kuliah
+
+
+SELECT * FROM Mahasiswa 
+SELECT a.nama_mahasiswa
+
+SELECT dosen_pengajar FROM jadwal_mata_kuliah
+WHERE dosen_pengajar LIKE 'ang%' 
